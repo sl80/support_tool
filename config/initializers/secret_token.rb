02b3512a88c4af4c510f188ec3022c671e3c5aee
Sebastian Lehbert <s.lehbert@gmail.com>
2011-02-19 00:47:47 +0100

    Rails 3 Application Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SupportTool::Application.config.secret_token = '07f677cf088eee7c6c099132a30c4f2f4833a37a8e2aef2ac68c64d22e7fdbd96f7b5e9ef2f91d7fbdc1fc7d86169c7ef0006f22806ff432cc8d439908aa0baa'
